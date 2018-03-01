7cd0aa40a5564edf0e7d6170ced55802b2b7255d
Dmitriy Krayniy <krdima92@gmail.com>
2018-03-01 15:41:12 +0300

    Scripts edited for Travis CI

--- publish.sh
+++ publish.sh
@@ -2,19 +2,20 @@
 
 COMPONENT=$(grep -m1 name package.json | tr -d '\r' | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 VERSION=$(grep -m1 version package.json | tr -d '\r' | awk -F: '{ print $2 }' | sed 's/[", ]//g')
-IMAGE1="pipdevs/${COMPONENT}:${VERSION}-${BUILD_NUMBER-0}"
+IMAGE1="pipdevs/${COMPONENT}:${VERSION}-${TRAVIS_BUILD_NUMBER}"
 IMAGE2="pipdevs/${COMPONENT}:latest"
-TAG="v${VERSION}-${BUILD_NUMBER-0}"
+TAG="v${VERSION}-${TRAVIS_BUILD_NUMBER}"
 
 # Any subsequent(*) commands which fail will cause the shell scrupt to exit immediately
 set -e
 set -o pipefail
 
 # Set tag on git repo
-git tag $TAG
-git push --tags
+#git tag $TAG
+#git push --tags
 
 # Push production image to docker registry
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+#docker login -u $DOCKER_USER -p $DOCKER_PASS
+cat docker/my_password.txt | docker login --username $DOCKER_USER --password-stdin
 docker push $IMAGE1
 docker push $IMAGE2
