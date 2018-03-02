fe5d94fa7578fd4dfb5961b70a2b167d22aa7003
Dmitriy Krayniy <krdima92@gmail.com>
2018-03-02 15:30:10 +0300

    added stage deploy scripts

---
+++ deploy_scripts/mark-stable.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo $TEST_PASSED
+if [ $TEST_PASSED == "false" ]
+then
+    echo Test failed. This step skipped.
+    exit 0
+fi
+
+COMPONENT=$(grep -m1 name ../package.json | tr -d '\r' | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+VERSION=$(grep -m1 version ../package.json | tr -d '\r' | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+BUILD_NUMBER=$(grep BUILD_NUMBER upgrade-stage.sh | grep rc | sed 's/[^0-9]//g')
+IMAGE="pipdevs/${COMPONENT}:${VERSION}-${BUILD_NUMBER}"
+TAG="v${VERSION}-${BUILD_NUMBER}"
+
+# Any subsequent(*) commands which fail will cause the shell scrupt to exit immediately
+set -e
+set -o pipefail
+
+# Set tag on git repo
+git tag $TAG
+git push --tags
+
+# Build docker image
+docker build -f ../docker/Dockerfile -t ${IMAGE} .
+
+# Push production image to docker registry
+cat docker/my_password.txt | docker login --username $DOCKER_USER --password-stdin
+docker push $IMAGE
