4259834403e3fc279d0bb308aff74d42f1970f62
Dmitriy Krayniy <krdima92@gmail.com>
2018-03-05 14:33:34 +0300

    fixed deploy script

--- deploy_scripts/mark-stable.sh
+++ deploy_scripts/mark-stable.sh
@@ -9,7 +9,7 @@ fi
 
 COMPONENT=$(grep -m1 name package.json | tr -d '\r' | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 VERSION=$(grep -m1 version package.json | tr -d '\r' | awk -F: '{ print $2 }' | sed 's/[", ]//g')
-BUILD_NUMBER=$(grep BUILD_NUMBER upgrade-stage.sh | grep rc | sed 's/[^0-9]//g')
+BUILD_NUMBER=$(grep BUILD_NUMBER deploy_scripts/upgrade-stage.sh | grep rc | sed 's/[^0-9]//g')
 IMAGE="pipdevs/${COMPONENT}:${VERSION}-${BUILD_NUMBER}"
 TAG="v${VERSION}-${BUILD_NUMBER}"
 
