39821624503d03ca6999004bccbdb9f04b618959
Dmitriy Krayniy <krdima92@gmail.com>
2018-03-05 16:13:51 +0300

    updated e2e test by cmdlets

--- deploy_scripts/test-stage.sh
+++ deploy_scripts/test-stage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker exec iqs-e2e powershell ~/tracker-ps-ws/tracker-simulation-ps/src/Test-StageDashboards.ps1
+sudo docker exec iqs-e2e powershell ~/tracker-ps-ws/tracker-simulation-ps/src/Test/Test-IqsDashboards.ps1 -ServerUrl "api.positron.stage.iquipsys.net"
