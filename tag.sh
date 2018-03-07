6b471db82bc886da792dc03e283fcf0300253043
Dmitriy Krayniy <krdima92@gmail.com>
2018-03-07 19:13:10 +0300

    renamed some files and folders

--- tag.sh
+++ tag.sh
@@ -5,7 +5,6 @@ COMPONENT=$(grep -m1 name package.json | tr -d '\r' | awk -F: '{ print $2 }' | s
 VERSION=$(grep -m1 version package.json | tr -d '\r' | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 TAG="v${VERSION}-${TRAVIS_BUILD_NUMBER}-rc"
 
-
 # Update build number in upgrade-stage.sh
 sed -i -e "s/BUILD_NUMBER=[0-9]*-rc/BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}-rc/g" ./deploy_scripts/upgrade-stage.sh
 
@@ -22,7 +21,6 @@ git remote add origin "https://stee1:${GITHUB_API_KEY}@github.com/pip-services-c
 git add ./deploy_scripts/upgrade-stage.sh
 git commit -m "Travis build ${TRAVIS_BUILD_NUMBER} updated build number in stage deploy file [skip ci]" 
 git push origin master
-
 
 # Set git tag
 git tag ${TAG}
