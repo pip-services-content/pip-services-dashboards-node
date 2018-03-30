bf8b49552d18dc5fc50ddd45908b4eb13b80676f
Dmitriy Krayniy <krdima92@gmail.com>
2018-03-30 11:29:03 +0300

    removed tag.sh from build because of infinite loop

--- tag.sh
+++ tag.sh
@@ -8,11 +8,14 @@ TAG="v${VERSION}-${TRAVIS_BUILD_NUMBER}-rc"
 # Configure git
 git config --global user.email "krdima92@gmail.com" 
 git config --global user.name "stee1" 
+#git config --global user.email "pipdevs@gmail.com" 
+#git config --global user.name "pipdeveloper" 
 
 #git checkout master
 
 git remote rm origin 
 git remote add origin "https://stee1:${GITHUB_API_KEY}@github.com/pip-services-content/${COMPONENT}.git"
+#git remote add origin "https://pipdeveloper:${GITHUB_API_KEY}@github.com/pip-services-content/${COMPONENT}.git"
 
 # Set git tag
 git tag ${TAG} -a -m "Generated tag from TravisCI for build #$TRAVIS_BUILD_NUMBER"
