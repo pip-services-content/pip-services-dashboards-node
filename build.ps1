5fd7d40b8289e77454263ad456e41230e75c5506
Dmitriy Krayniy <krdima92@gmail.com>
2018-05-02 12:33:48 +0300

    build scripts changed to powershell cmdlets

---
+++ build.ps1
@@ -0,0 +1,21 @@
+#!/usr/bin/env pwsh
+
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop"
+
+$component = Get-Content -Path "component.json" | ConvertFrom-Json
+$buildImage="$($component.registry)/$($component.name):$($component.version)-build"
+$container=$component.name
+
+# Remove build files
+if (Test-Path "obj") {
+    Remove-Item -Recurse -Force -Path "obj"
+}
+
+# Build docker image
+docker build -f docker/Dockerfile.build -t $buildImage .
+
+# Create and copy compiled files, then destroy
+docker create --name $container $buildImage
+docker cp "$($container):/obj" ./obj
+docker rm $container
