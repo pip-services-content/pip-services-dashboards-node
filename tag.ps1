5abdee25ff0fc7308f213af45056f065f02fa918
Dmitriy Krayniy <krdima92@gmail.com>
2018-05-02 15:30:19 +0300

    fixed tag script

--- tag.ps1
+++ tag.ps1
@@ -11,8 +11,8 @@ git config --global user.email "pipdevs@gmail.com"
 git config --global user.name "pipdeveloper" 
 
 git remote rm origin 
-git remote add origin "https://pipdeveloper:$($env:GITHUB_API_KEY)@github.com/pip-services-content/$($env:COMPONENT).git"
+git remote add origin "https://pipdeveloper:$($env:GITHUB_API_KEY)@github.com/pip-services-content/$($component.name).git"
 
 # Set git tag
-git tag $env:TAG -a -m "Generated tag from GitLabCI for build #$($component.build)"
+git tag "$($env:TAG)" -a -m "Generated tag from GitLabCI for build #$($component.build)"
 git push --tags
