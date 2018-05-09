0b3fa625cd229e7bfb6095cd6d0b72a3ed5974df
Dmitriy Krayniy <krdima92@gmail.com>
2018-05-09 10:34:46 +0300

    changed git remote

--- tag.ps1
+++ tag.ps1
@@ -13,7 +13,8 @@ git config --global user.name "pipdeveloper"
 
 #git remote rm origin 
 #git remote add origin "https://pipdeveloper:$($env:GITHUB_API_KEY)@github.com/pip-services-content/$($component.name).git"
-git remote set-url "https://pipdeveloper:$($env:GITHUB_API_KEY)@github.com/pip-services-content/$($component.name).git"
+#git remote set-url "https://pipdeveloper:$($env:GITHUB_API_KEY)@github.com/pip-services-content/$($component.name).git"
+git remote set-url origin "git@github.com:pip-services-content/$($component.name).git"
 
 # Set git tag
 git tag $tag -a -m "Generated tag from GitLabCI for build #$($env:TRAVIS_BUILD_NUMBER)"
