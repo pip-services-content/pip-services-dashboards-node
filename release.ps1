9d7f8b2bf43e5fa04da109bfb037c1d62d3f620c
Dmitriy Krayniy <krdima92@gmail.com>
2018-05-10 11:22:08 +0300

    test changes

--- release.ps1
+++ release.ps1
@@ -11,13 +11,15 @@ if ($component.version -ne $version) {
     throw "Versions in component.json and package.json do not match"
 }
 
-npm whoami
+Write-Host "Token is $($env:NPM_TOKEN)"
 
 # Automatically login to server
 if ($env:NPM_USER -ne $null -and $env:NPM_PASS -ne $null -and $env:NPM_EMAIL -ne $null) {
     npm-cli-login
 } elseif ($env:NPM_TOKEN -ne $null) {
+    Write-Host "Setting npmrc file"
     Set-Content -Path .npmrc -Value "//registry.npmjs.org/:_authToken=\$($env:NPM_TOKEN)"
+    Get-Content -Path ".npmrc" | Write-Host "Content is: $_"
 }
 
 npm whoami
