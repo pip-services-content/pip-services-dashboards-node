26e26797928fb3c14c66bfd9b08d8c940d4db586
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 14:02:23 +0300

    added DefaultRpcFactory

--- obj/src/container/DashboardsProcess.js
+++ obj/src/container/DashboardsProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const DashboardsServiceFactory_1 = require("../build/DashboardsServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class DashboardsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("dashboards", "Application dashboards microservice");
         this._factories.add(new DashboardsServiceFactory_1.DashboardsServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.DashboardsProcess = DashboardsProcess;
--- src/container/DashboardsProcess.ts
+++ src/container/DashboardsProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { DashboardsServiceFactory } from '../build/DashboardsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class DashboardsProcess extends ProcessContainer {
 
     public constructor() {
         super("dashboards", "Application dashboards microservice");
         this._factories.add(new DashboardsServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
